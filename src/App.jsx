26a7b997ab9becdc34eb4999f74726cc957ebb21
shovonkumar <mail.shovon2@gmail.com>
2023-09-09 01:20:27 +0600

    files added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Banner from "./components/banner/Banner";
+
+function App() {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <section className="banner">
+      <h1>
+        your life <span>,</span> a photo
+      </h1>
+    </section>
+  );
+};
+
+export default Banner;
